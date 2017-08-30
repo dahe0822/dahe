baddf5dc125feb3079672c9e282066e8bc6ac2ba
dahyekim97 <dh47kim@edu.uwaterloo.ca>
2017-08-30 19:46:29 -0400

    -

---
+++ js/hobby.js
@@ -0,0 +1,51 @@
+$(document).ready(function(ev){
+    var items = $(".nav li").length;
+    var leftRight=0;
+    if(items>5){
+        leftRight=(items-5)*50*-1;
+    }
+    $('#custom_carousel').on('slide.bs.carousel', function (evt) {
+        
+ 
+      $('#custom_carousel .controls li.active').removeClass('active');
+      $('#custom_carousel .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+    })
+    $('.nav').draggable({ 
+        axis: "x",
+         stop: function() {
+            var ml = parseInt($(this).css('left'));
+            if(ml>0)
+            $(this).animate({left:"0px"});
+                if(ml<leftRight)
+                    $(this).animate({left:leftRight+"px"});
+                    
+        }
+      
+    });
+});
+
+$(document).ready(function(ev){
+    var items = $(".nav_2 li").length;
+    var leftRight=0;
+    if(items>5){
+        leftRight=(items-5)*50*-1;
+    }
+    $('#custom_carousel_2').on('slide.bs.carousel', function (evt) {
+        
+ 
+      $('#custom_carousel_2 .controls li.active').removeClass('active');
+      $('#custom_carousel_2 .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+    })
+    $('.nav_2').draggable({ 
+        axis: "x",
+         stop: function() {
+            var ml = parseInt($(this).css('left'));
+            if(ml>0)
+            $(this).animate({left:"0px"});
+                if(ml<leftRight)
+                    $(this).animate({left:leftRight+"px"});
+                    
+        }
+      
+    });
+});
